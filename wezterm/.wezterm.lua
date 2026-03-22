74c4d439cf1b1f43dd82c2bdd22ea63b6df5a5a9
tinhtt <tinhtrantrung.vn@gmail.com>
2026-03-22 23:59:11 +1300

    :package: Use different font size for wezterm on different os.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,11 +1,28 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Table to map OS patterns to font sizes
+local font_sizes = {
+	windows = 10.0,
+	apple = 14.0,
+	linux = 12.0, -- Optional: added for completeness
+}
+
+-- Default size if no match is found
+config.font_size = 12.0
+
+-- Loop through the table to find a match in the target_triple
+for platform, size in pairs(font_sizes) do
+	if wezterm.target_triple:find(platform) then
+		config.font_size = size
+		break
+	end
+end
+
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("FiraMono Nerd Font")
-config.font_size = 10.0
 
-config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
