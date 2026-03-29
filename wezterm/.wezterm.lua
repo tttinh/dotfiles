3fa282a335cc43c9f2fd7973a02af09e8a046de2
tinhtt <tinhtrantrung.vn@gmail.com>
2026-03-29 23:26:44 +1300

    Revert ":package: Update wezterm key bindings."
    This reverts commit c3e9ab1a836352cb0f95159ce76e7afa6566e23f.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local act = wezterm.action
 
 -- Table to map OS patterns to font sizes
 local font_sizes = {
@@ -28,25 +27,5 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
-
--- Set the leader key to Shift+Space (like tmux)
-config.leader = { key = "Space", mods = "SHIFT", timeout_milliseconds = 2000 }
-config.keys = {
-	-- Move between WezTerm panes
-	{ mods = "META", key = "h", action = act.ActivatePaneDirection("Left") },
-	{ mods = "META", key = "j", action = act.ActivatePaneDirection("Down") },
-	{ mods = "META", key = "k", action = act.ActivatePaneDirection("Up") },
-	{ mods = "META", key = "l", action = act.ActivatePaneDirection("Right") },
-
-	-- Splitting (Leader + - for horizontal, Leader + \ for vertical)
-	{ mods = "META", key = "-", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "META", key = "\\", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-
-	-- Closing
-	{ mods = "META", key = "d", action = act.CloseCurrentPane({ confirm = false }) },
-
-	-- Enter copy mode
-	{ mods = "LEADER", key = "[", action = act.ActivateCopyMode },
-}
 
 return config
